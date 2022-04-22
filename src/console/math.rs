0be5556a26f0bd6241768c667a9591374f2ea2be
Christian Fischer <christian@chfi.se>
2022-04-21 19:32:47 -0500

    getters & setters for 2, 3, 4D vecs

--- src/console/math.rs
+++ src/console/math.rs
@@ -38,6 +38,74 @@ use lazy_static::lazy_static;
 pub fn create_rhai_module() -> rhai::Module {
     let mut module: rhai::Module = rhai::exported_module!(rhai_module);
 
+    // module.set_iter(type_id, func)
+    module.set_native_fn("add", |a: &mut [f32; 2], b: [f32; 2]| {
+        Ok([a[0] + b[0], a[1] + b[1]])
+    });
+    // module.set_iterator::<[f32; 2]>();
+
+    macro_rules! vec_math_fns {
+        ($ty:ty) => {
+            module.set_native_fn("add", |a: &mut [$ty; 2], b: [$ty; 2]| {
+                Ok([a[0] + b[0], a[1] + b[1]])
+            });
+            module.set_native_fn("add", |a: &mut [$ty; 3], b: [$ty; 3]| {
+                Ok([a[0] + b[0], a[1] + b[1], a[2] + b[2]])
+            });
+            module.set_native_fn("add", |a: &mut [$ty; 4], b: [$ty; 4]| {
+                Ok([a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]])
+            });
+
+            module.set_native_fn("sub", |a: &mut [$ty; 2], b: [$ty; 2]| {
+                Ok([a[0] - b[0], a[1] - b[1]])
+            });
+            module.set_native_fn("sub", |a: &mut [$ty; 3], b: [$ty; 3]| {
+                Ok([a[0] - b[0], a[1] - b[1], a[2] - b[2]])
+            });
+            module.set_native_fn("sub", |a: &mut [$ty; 4], b: [$ty; 4]| {
+                Ok([a[0] - b[0], a[1] - b[1], a[2] - b[2], a[3] - b[3]])
+            });
+        };
+    }
+
+    module.set_native_fn("vec4", |x: f32, y: f32, z: f32, w: f32| {
+        Ok([x, y, z, w])
+    });
+
+    macro_rules! vec_get_set {
+        ($arr:ty, $ty:ty, [$(($name:literal, $ix:literal)),*]) => {
+            $(
+                module.set_getter_fn($name, |v: &mut $arr| Ok(v[$ix]));
+                module.set_setter_fn($name, |v: &mut $arr, x: $ty| {
+                    v[$ix] = x;
+                    Ok(())
+                });
+            )*
+        };
+    }
+
+    macro_rules! impl_get_set {
+        ($ty:ty) => {
+            vec_get_set!([$ty; 2], $ty, [("x", 0), ("y", 1)]);
+            vec_get_set!([$ty; 3], $ty, [("x", 0), ("y", 1), ("z", 2)]);
+            vec_get_set!(
+                [$ty; 4],
+                $ty,
+                [("x", 0), ("y", 1), ("z", 2), ("w", 3)]
+            );
+        };
+    }
+
+    impl_get_set!(f32);
+    impl_get_set!(u32);
+    impl_get_set!(i32);
+
+    vec_math_fns!(f32);
+    vec_math_fns!(u32);
+    vec_math_fns!(i32);
+
+    module.set_getter_fn("x", |v: &mut [f32; 2]| Ok(v[0]));
+
     // macro_rules! {
     // module.
     // }
@@ -59,49 +127,4 @@ pub mod rhai_module {
     pub type UVec2 = [u32; 2];
     pub type UVec3 = [u32; 3];
     pub type UVec4 = [u32; 4];
-
-    #[rhai_fn(pure, get = "x")]
-    pub fn vec2_get_x(v: &mut Vec2) -> f32 {
-        v[0]
-    }
-
-    #[rhai_fn(pure, get = "y")]
-    pub fn vec2_get_y(v: &mut Vec2) -> f32 {
-        v[1]
-    }
-
-    #[rhai_fn(pure, get = "x")]
-    pub fn vec3_get_x(v: &mut Vec3) -> f32 {
-        v[0]
-    }
-
-    #[rhai_fn(pure, get = "y")]
-    pub fn vec3_get_y(v: &mut Vec3) -> f32 {
-        v[1]
-    }
-
-    #[rhai_fn(pure, get = "z")]
-    pub fn vec3_get_z(v: &mut Vec3) -> f32 {
-        v[2]
-    }
-
-    #[rhai_fn(pure, get = "x")]
-    pub fn vec4_get_x(v: &mut Vec4) -> f32 {
-        v[0]
-    }
-
-    #[rhai_fn(pure, get = "y")]
-    pub fn vec4_get_y(v: &mut Vec4) -> f32 {
-        v[1]
-    }
-
-    #[rhai_fn(pure, get = "z")]
-    pub fn vec4_get_z(v: &mut Vec4) -> f32 {
-        v[2]
-    }
-
-    #[rhai_fn(pure, get = "w")]
-    pub fn vec4_get_w(v: &mut Vec4) -> f32 {
-        v[3]
-    }
 }
