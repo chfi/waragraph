b04c68605ec5f96cfbcd5ae0e951233d4b9c503d
Christian Fischer <christian@chfi.se>
2022-05-02 13:47:46 -0500

    partial BED parsing

--- src/console/data.rs
+++ src/console/data.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, num::NonZeroU32};
+use std::{
+    collections::{BTreeMap, HashMap},
+    io::BufReader,
+    num::NonZeroU32,
+};
 
 use ash::vk;
 use bstr::ByteSlice;
@@ -28,6 +32,7 @@ use anyhow::{anyhow, bail, Result};
 use bstr::ByteSlice as BstrByteSlice;
 
 use crate::{
+    graph::{Node, Path, Waragraph},
     util::{BufFmt, BufId, BufMeta, BufferStorage, LabelStorage},
     viewer::{DataSource, SlotFnCache, ViewDiscrete1D},
 };
@@ -37,6 +42,134 @@ use rhai::plugin::*;
 use lazy_static::lazy_static;
 
 use self::rhai_module::DataSourceF32;
+
+#[derive(Clone)]
+pub enum BedColumn {
+    Int(Vec<i64>),
+    Float(Vec<f32>),
+    String(Vec<rhai::ImmutableString>),
+    Dyn(Vec<rhai::Dynamic>),
+}
+
+pub struct AnnotationSet {
+    // e.g. BED file path
+    source: rhai::ImmutableString,
+
+    // records: Vec<BEDRecord>,
+
+    // map from path to a map from nodes to bitmaps representing the
+    // record indices at each node
+    path_record_indices:
+        FxHashMap<Path, BTreeMap<Node, roaring::RoaringBitmap>>,
+
+    path_record_nodes: FxHashMap<Path, BTreeMap<usize, roaring::RoaringBitmap>>,
+    // columns: Vec<Vec<
+    column_headers: FxHashMap<rhai::ImmutableString, usize>,
+    columns: Vec<BedColumn>,
+}
+
+impl AnnotationSet {
+    pub fn load_bed<P: AsRef<std::path::Path>>(
+        graph: &Arc<Waragraph>,
+        path: P,
+    ) -> Result<AnnotationSet> {
+        use std::fs::File;
+        use std::io::prelude::*;
+
+        let path = path.as_ref();
+
+        let source = path
+            .to_str()
+            .map(rhai::ImmutableString::from)
+            .unwrap_or("UNKNOWN".into());
+
+        let mut path_record_indices: FxHashMap<
+            Path,
+            BTreeMap<Node, roaring::RoaringBitmap>,
+        > = FxHashMap::default();
+
+        let mut path_record_nodes: FxHashMap<
+            Path,
+            BTreeMap<usize, roaring::RoaringBitmap>,
+        > = FxHashMap::default();
+
+        let mut column_headers = FxHashMap::default();
+
+        let mut columns = Vec::new();
+
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        let mut header_done = false;
+        let mut ix = 0;
+
+        for line in reader.lines() {
+            let line = line?;
+
+            if !header_done {
+                if line.starts_with("#") {
+                    //
+                } else if line.starts_with("browser") {
+                    //
+                } else if line.starts_with("track") {
+                    //
+                } else {
+                    header_done = true;
+                }
+            }
+
+            if header_done {
+                let mut fields = line.split("\t");
+
+                let path_name = fields.next().unwrap();
+                let start = fields.next().unwrap().parse::<usize>()?;
+                let end = fields.next().unwrap().parse::<usize>()?;
+
+                if let Some(path) = graph.path_index(path_name.as_bytes()) {
+                    let offset = graph.path_offset(path);
+                    let start = start - offset;
+                    let end = end - offset;
+
+                    let indices = path_record_indices.entry(path).or_default();
+                    let nodes = path_record_nodes.entry(path).or_default();
+
+                    for &(node, _) in
+                        graph.nodes_in_path_range(path, start..end)
+                    {
+                        indices.entry(node).or_default().insert(ix as u32);
+                        nodes.entry(ix).or_default().insert(node.into());
+                    }
+                }
+
+                ix += 1;
+            }
+        }
+
+        Ok(AnnotationSet {
+            source,
+            path_record_indices,
+            path_record_nodes,
+            column_headers,
+            columns,
+        })
+    }
+
+    pub fn path_records(
+        &self,
+        path: Path,
+    ) -> Option<&BTreeMap<Node, roaring::RoaringBitmap>> {
+        self.path_record_indices.get(&path)
+    }
+
+    pub fn path_node_records(
+        &self,
+        path: Path,
+        node: Node,
+    ) -> Option<&roaring::RoaringBitmap> {
+        let path = self.path_record_indices.get(&path)?;
+        path.get(&node)
+    }
+}
 
 pub fn create_rhai_module() -> rhai::Module {
     let mut module: rhai::Module = rhai::exported_module!(rhai_module);
