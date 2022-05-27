63b552905b91fd0eced035330a5e9c3eaa85153e
Christian Fischer <christian@chfi.se>
2022-05-27 12:32:00 -0500

    decouple buffer cache from allocation

--- src/viewer/cache.rs
+++ src/viewer/cache.rs
@@ -24,24 +24,26 @@ use std::sync::Arc;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CacheError {
     OutOfRows,
-    RowSizeMismatch { actual: usize, expected: usize },
     ElemSizeMismatch,
+    RowSizeMismatch { actual: usize, expected: usize },
+    BufferSizeMismatch { actual: usize, expected: usize },
 }
 
 impl std::fmt::Display for CacheError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             CacheError::OutOfRows => {
-                write!(f, "Buffer cache allocation error: Unbound row")
+                write!(f, "Buffer cache allocation error: Out of rows, need reallocation")
             }
             CacheError::RowSizeMismatch { actual, expected } => {
                 write!(f, "Buffer cache update error: Data consisted of {} elements, but row expected {}", actual, expected)
             }
             CacheError::ElemSizeMismatch => {
                 write!(f, "Buffer cache update error: Data bytestring not evenly divisible with element size")
-            } // CacheError::ElemSizeMismatch { actual, expected } => {
-              //     write!(f, "Buffer cache update error: Data does not consist of  {}, but cache expected {}", actual, expected)
-              // }
+            }
+            CacheError::BufferSizeMismatch { actual, expected } => {
+                write!(f, "Buffer cache update error: Provided buffer is {} bytes, expected {}", actual, expected)
+            }
         }
     }
 }
@@ -52,37 +54,53 @@ pub struct BufferCache<K>
 where
     K: std::hash::Hash + Eq,
 {
-    usage: vk::BufferUsageFlags,
+    // usage: vk::BufferUsageFlags,
+    // buffer: BufferIx,
+    // desc_set: DescSetIx,
+    row_map: HashMap<K, usize>,
 
     elem_size: usize,
     row_size: usize,
 
     row_capacity: usize,
-
-    buffer: BufferIx,
-    desc_set: DescSetIx,
-    // updates_tx: crossbeam::channel::Sender<()>,
-    // updates_rx: crossbeam::channel::Receiver<()>,
-    // row_map: HashMap<K, Range<usize>>,
-    row_map: HashMap<K, usize>,
-
+    used_row_count: usize,
     used_rows: Vec<bool>,
     // used_rows: roaring::RoaringBitmap,
 }
 
 impl<K: std::hash::Hash + Eq> BufferCache<K> {
     pub fn new(
-        engine: &mut VkEngine,
+        // engine: &mut VkEngine,
         elem_size: usize,
         row_size: usize,
-        row_count: usize,
-    ) -> Result<Self> {
-        todo!();
+        row_capacity: usize,
+    ) -> Self {
+        Self {
+            row_map: HashMap::default(),
+
+            elem_size,
+            row_size,
+
+            row_capacity,
+            used_row_count: 0,
+            used_rows: vec![false; row_capacity],
+        }
     }
 
     pub fn clear(&mut self) {
         self.row_map.clear();
         self.used_rows.iter_mut().for_each(|v| *v = false);
+        self.used_row_count = 0;
+    }
+
+    pub fn reallocate_rows(&mut self, row_count: usize) {
+        self.clear();
+        self.used_rows.resize(row_count, false);
+    }
+
+    pub fn resize_rows(&mut self, new_width: usize) {
+        self.clear();
+        self.row_size = new_width;
     }
 
     pub fn is_bound<Q: ?Sized>(&self, k: &Q) -> bool
@@ -128,6 +146,7 @@ impl<K: std::hash::Hash + Eq> BufferCache<K> {
         self.used_rows[row_ix] = true;
 
         self.row_map.insert(k, row_ix);
+        self.used_row_count += 1;
 
         Ok(self.range_for_ix(row_ix))
     }
@@ -138,7 +157,12 @@ impl<K: std::hash::Hash + Eq> BufferCache<K> {
         Q: std::hash::Hash + Eq,
     {
         let row_ix = self.row_map.remove(k)?;
+        debug_assert!(
+            self.used_rows[row_ix],
+            "Buffer cache: Row map entry existed but row was not in use"
+        );
         self.used_rows[row_ix] = false;
+        self.used_row_count -= 1;
         Some(())
     }
 
@@ -148,37 +172,47 @@ impl<K: std::hash::Hash + Eq> BufferCache<K> {
 
     pub fn write_row<Q: ?Sized>(
         &mut self,
-        res: &mut GpuResources,
+        buffer: &mut [u8],
+        // buffer: &mut BufferRes,
+        // res: &mut GpuResources,
         row: &Q,
         data: &[u8],
-    ) -> std::result::Result<(), CacheError>
+        // ) -> std::result::Result<(), CacheError>
+    ) -> Result<()>
     where
         K: Borrow<Q>,
-        Q: std::hash::Hash + Eq,
+        Q: std::hash::Hash + Eq + std::fmt::Debug,
     {
-        if data.len() % self.elem_size != 0 {
-            return Err(CacheError::ElemSizeMismatch);
+        let elem_count = data.len() / self.elem_size;
+
+        let total_size = self.elem_size * self.row_size * self.row_capacity;
+
+        if buffer.len() != total_size {
+            return Err(CacheError::BufferSizeMismatch {
+                actual: buffer.len(),
+                expected: total_size,
+            }
+            .into());
         }
 
-        let elem_count = data.len() / self.elem_size;
+        if data.len() % self.elem_size != 0 {
+            return Err(CacheError::ElemSizeMismatch.into());
+        }
 
         if elem_count != self.row_size {
             return Err(CacheError::RowSizeMismatch {
                 actual: elem_count,
                 expected: self.row_size,
-            });
+            }
+            .into());
         }
 
-        let range = if let Some(range) = self.row_map.get(row) {
-            range
-        } else {
-            todo!();
-        };
+        let range = self
+            .get_range(row)
+            .ok_or(anyhow!("Buffer cache error: Unbound key {:?}", row))?;
 
-        let buffer = &mut res[self.buffer]
-            .mapped_slice_mut()
-            .expect("BufferCache can't be bound, impossible!");
+        buffer[range].clone_from_slice(data);
 
-        todo!();
+        Ok(())
     }
 }
