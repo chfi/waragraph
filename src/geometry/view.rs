74c4664182aea3275632a10fdf1c00d54def36d8
Christian Fischer <christian@chfi.se>
2022-06-06 13:46:38 -0500

    use euclid::Length internally in View1D

--- src/geometry/view.rs
+++ src/geometry/view.rs
@@ -17,8 +17,9 @@ use num_traits::{
     one, zero, AsPrimitive, FromPrimitive, Num, NumOps, One, ToPrimitive,
 };
 
+#[derive(Debug)]
 pub struct PangenomeSpace;
-pub type Bp = Length<usize, PangenomeSpace>;
+// pub type Bp = Length<usize, PangenomeSpace>;
 
 pub struct ScreenSpace;
 pub type ScreenLen = Length<f32, ScreenSpace>;
@@ -28,228 +29,46 @@ pub type PixelsLen = Length<usize, ScreenSpace>;
 
 pub type PangenomeScreenScale<T> = Scale<T, PangenomeSpace, ScreenSpace>;
 
-pub type PangenomeView = View1D<Bp>;
+pub type PangenomeView = View1D<usize, PangenomeSpace>;
 
+/*
 impl View1D<Bp> {
     pub fn screen_scale(&self, pixel_len: f64) -> PangenomeScreenScale<f64> {
         let f = pixel_len / self.len.0 as f64;
         Scale::new(f)
     }
 }
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct View1D<I>
-where
-    I: Copy + PartialEq + PartialOrd + Add<Output = I> + euclid::num::Zero,
-{
-    max: I,
-
-    offset: I,
-    len: I,
-}
-
-impl<I> View1D<I>
-where
-    I: Copy + PartialEq + PartialOrd + Add<Output = I> + euclid::num::Zero,
-{
-    pub fn new(max: I) -> Self {
-        Self {
-            max,
-            offset: I::zero(),
-            len: max,
-        }
-    }
-
-    pub fn offset(&self) -> I {
-        self.offset
-    }
-
-    pub fn len(&self) -> I {
-        self.len
-    }
-
-    pub fn max(&self) -> I {
-        self.max
-    }
-
-    pub fn is_valid(&self) -> bool {
-        self.len > I::zero() && (self.offset + self.len <= self.max)
-    }
-
-    pub fn reset(&mut self) {
-        self.offset = I::zero();
-        self.len = self.max;
-    }
-
-    pub fn set(&mut self, offset: I, len: I) {
-        assert!(len > I::zero());
-        assert!(offset + len <= self.max);
-        self.offset = offset;
-        self.len = len;
-    }
-}
-
-//
-impl<I> View1D<I>
-where
-    I: Copy
-        + PartialEq
-        + PartialOrd
-        + Add<Output = I>
-        + Sub<Output = I>
-        + euclid::num::Zero,
-{
-    pub fn set_offset(&self, new_offset: I) -> Self {
-        let mut new = *self;
-
-        if new_offset + self.len >= self.max {
-            new.offset = self.max - self.len;
-        } else {
-            new.offset = new_offset;
-        }
-
-        new
-    }
-
-    pub fn shift_right(&self, delta: I) -> Self {
-        let mut new = *self;
-
-        if delta + self.offset + self.len >= self.max {
-            new.offset = self.max - self.len;
-        } else {
-            new.offset = self.offset + delta;
-        }
-
-        new
-    }
-
-    pub fn shift_left(&self, delta: I) -> Self {
-        let mut new = *self;
-
-        if delta >= self.offset {
-            new.offset = I::zero();
-        } else {
-            new.offset = self.offset - delta;
-        }
-
-        new
-    }
-
-    /// Returns a new `View1D<I>` with the same offset but a new length.
-    pub fn resize_from_left(&self, new_len: I) -> Self {
-        if self.offset + new_len >= self.max {
-            Self {
-                len: self.max - self.offset,
-                ..*self
-            }
-        } else {
-            Self {
-                len: new_len,
-                ..*self
-            }
-        }
-    }
-
-    pub fn resize_around<X>(&self, p: I, new_len: I) -> Self
-    where
-        I: std::ops::Div<Output = X> + std::ops::Mul<X>,
-        X: ToPrimitive,
-    {
-        if new_len > self.len {
-            // "zooming out"
-            let fact = new_len / self.len;
-
-            // let f_a = p / self.len;
-            // let f_b = p / new_len;
-
-            todo!();
-        } else if new_len < self.len {
-            // "zooming in"
-            let fact = self.len / new_len;
-
-            todo!();
-        } else {
-            *self
-        }
-    }
-
-    /*
-    /// Returns a new `View1D<I>` by resizing this view while keeping the right-hand side fixed.
-    pub fn resize_from_right(&self, new_len: I) -> Self {
-    }
-    */
-}
-
-/*
-impl<I> View1D<I>
-where
-    I: Copy
-        + PartialEq
-        + PartialOrd
-        + Add<Output = I>
-        + Sub<Output = I>
-        + euclid::num::Zero,
-{
-    pub fn set_offset(&self, new_offset: I) -> Self {
-        let mut new = *self;
-
-        if new_offset + self.len >= self.max {
-            new.offset = self.max - self.len;
-        } else {
-            new.offset = new_offset;
-        }
-
-        new
-    }
-
-    pub fn shift_right(&self, delta: I) -> Self {
-        let mut new = *self;
-
-        if delta + self.offset + self.len >= self.max {
-            new.offset = self.max - self.len;
-        } else {
-            new.offset = self.offset + delta;
-        }
-
-        new
-    }
-
-    pub fn shift_left(&self, delta: I) -> Self {
-        let mut new = *self;
-
-        if delta >= self.offset {
-            new.offset = I::zero();
-        } else {
-            new.offset = self.offset - delta;
-        }
-
-        new
-    }
-}
 */
 
-/*
-pub type PgView = View1D_<usize, PangenomeSpace>;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct View1D_<I, U>
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct View1D<I, U = UnknownUnit>
 where
-    I: Copy + PartialEq + PartialOrd + NumOps + Zero,
-    // I: NumOps + One + Zero,
+    I: Clone + PartialOrd,
 {
+    max: Length<I, U>,
+
     offset: Length<I, U>,
     len: Length<I, U>,
-
-    max: Length<I, U>,
 }
 
-impl<I, U> View1D_<I, U>
+impl<I: Clone + PartialOrd, U> Clone for View1D<I, U> {
+    fn clone(&self) -> Self {
+        Self {
+            max: self.max.clone(),
+            offset: self.offset.clone(),
+            len: self.len.clone(),
+        }
+    }
+}
+
+impl<I: Copy + PartialOrd, U> Copy for View1D<I, U> {}
+
+impl<I, U> View1D<I, U>
 where
-    I: Copy + PartialEq + PartialOrd + NumOps + Zero,
+    I: Copy + PartialEq + PartialOrd + Add<Output = I> + num_traits::Zero,
 {
     pub fn new(max: I) -> Self {
         let max = Length::new(max);
-
         Self {
             max,
             offset: Length::new(I::zero()),
@@ -270,24 +89,159 @@ where
     }
 
     pub fn is_valid(&self) -> bool {
-        self.len > Length::new(zero()) && (self.offset + self.len <= self.max)
+        self.len.0 > euclid::num::Zero::zero()
+            && (self.offset + self.len <= self.max)
     }
 
     pub fn reset(&mut self) {
-        self.offset = Length::new(zero());
+        self.offset = euclid::num::Zero::zero();
         self.len = self.max;
     }
 
     pub fn set(&mut self, offset: I, len: I) {
+        let o = Length::new(offset);
+        let l = Length::new(len);
         assert!(len > I::zero());
-
-        let offset = Length::new(offset);
-        let len = Length::new(len);
-
-        assert!(offset + len <= self.max);
-
-        self.offset = offset;
-        self.len = len;
+        assert!(o + l <= self.max);
+        self.offset = o;
+        self.len = l;
     }
 }
-*/
+
+//
+
+impl<I, U> View1D<I, U>
+where
+    I: Copy
+        + PartialEq
+        + PartialOrd
+        + Add<Output = I>
+        + Sub<Output = I>
+        + num_traits::Zero,
+{
+    pub fn set_offset(&self, new_offset: I) -> Self {
+        let mut new = self.to_owned();
+
+        let new_offset = Length::new(new_offset);
+
+        if new_offset + new.len >= new.max {
+            new.offset = new.max - new.len;
+        } else {
+            new.offset = new_offset;
+        }
+
+        new
+    }
+
+    pub fn shift_right(&self, delta: I) -> Self {
+        let mut new = *self;
+
+        let delta = Length::new(delta);
+
+        if delta + self.offset + self.len >= self.max {
+            new.offset = self.max - self.len;
+        } else {
+            new.offset = self.offset + delta;
+        }
+
+        new
+    }
+
+    pub fn shift_left(&self, delta: I) -> Self {
+        let mut new = *self;
+
+        let delta = Length::new(delta);
+
+        if delta >= self.offset {
+            new.offset = euclid::num::Zero::zero();
+        } else {
+            new.offset = self.offset - delta;
+        }
+
+        new
+    }
+
+    /// Returns a new `View1D<I>` with the same offset but a new length.
+    pub fn resize_from_left(&self, new_len: I) -> Self {
+        let new_len = Length::new(new_len);
+        if self.offset + new_len >= self.max {
+            Self {
+                len: self.max - self.offset,
+                ..*self
+            }
+        } else {
+            Self {
+                len: new_len,
+                ..*self
+            }
+        }
+    }
+
+    /*
+    pub fn resize_around(&self, p: I, new_len: I) -> Self
+    where
+        I: ToPrimitive + FromPrimitive,
+    {
+        let p_f = p.to_f64().unwrap();
+        let ol_f = self.len.0.to_f64().unwrap();
+        let nl_f = new_len.to_f64().unwrap();
+
+        let new_len = Length::new(new_len);
+
+        dbg!(p_f);
+
+        // let mut new = self.shift_right(p);
+        let mut new = *self;
+        new.len = new_len;
+
+        if new_len > self.len {
+            // "zooming out"
+            let fact = nl_f / ol_f;
+            let rev_delta = I::from_f64(fact * p_f).unwrap();
+
+            dbg!(fact);
+            // dbg!(rev_delta);
+
+            new.shift_left(rev_delta)
+        } else if new_len < self.len {
+            // "zooming in"
+            let fact = ol_f / nl_f;
+            let rev_delta = I::from_f64(fact * p_f).unwrap();
+
+            dbg!(fact);
+            // dbg!(rev_delta);
+
+            new.shift_left(rev_delta)
+        } else {
+            *self
+        }
+    }
+    */
+
+    /*
+    /// Returns a new `View1D<I>` by resizing this view while keeping the right-hand side fixed.
+    pub fn resize_from_right(&self, new_len: I) -> Self {
+    }
+    */
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_view_zoom() -> anyhow::Result<()> {
+        // let mut view: PangenomeView = View1D::new(1_000_000);
+        let view: PangenomeView = View1D::new(10_000);
+
+        // let zoomed = view.resize_around(5_000, 5_000);
+        let zoomed = view.resize_from_left(5_000);
+
+        eprintln!("original: {:?}", view);
+        eprintln!("zoomed:   {:?}", zoomed);
+
+        assert!(false);
+
+        Ok(())
+    }
+}
