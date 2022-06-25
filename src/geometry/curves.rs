06c6f330352b7cc84cc5def9bc6b85858da3d34f
Christian Fischer <christian@chfi.se>
2022-06-24 22:44:47 -0500

    more methods

--- src/geometry/curves.rs
+++ src/geometry/curves.rs
@@ -21,15 +21,72 @@ use anyhow::Result;
 
 use ndarray::prelude::*;
 
-use crate::graph::{Node, Waragraph};
+use crate::graph::{Node, Path, Waragraph};
 
 use super::{ScreenPoint, ScreenRect, ScreenSize, ScreenSpace};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct CurveId(NonZeroI32);
+pub struct CurveId(u32);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct OrientedCurve(NonZeroI32);
+
+impl From<OrientedCurve> for CurveId {
+    fn from(oc: OrientedCurve) -> CurveId {
+        CurveId((oc.0.get().abs() - 1) as u32)
+    }
+}
+
+impl From<CurveId> for OrientedCurve {
+    fn from(CurveId(id): CurveId) -> OrientedCurve {
+        let id = unsafe { NonZeroI32::new_unchecked(1 + id as u32) };
+        OrientedCurve(id)
+    }
+}
+
+impl OrientedCurve {
+    pub fn id(&self) -> CurveId {
+        CurveId((self.0.get() - 1) as u32)
+    }
+
+    pub fn is_reverse(&self) -> bool {
+        // self.0.get().is_negative()
+        self.0.leading_zeros() == 0
+    }
+
+    pub fn new(node_id: u32, is_reverse: bool) -> Self {
+        let mut id = 1 + node_id as i32;
+
+        if is_reverse {
+            id *= -1;
+        }
+
+        let id = unsafe { NonZeroI32::new_unchecked(id) };
+
+        Self(id)
+    }
+
+    pub fn flip(self) -> Self {
+        let id = unsafe { NonZeroI32::new_unchecked(self.0.get() * -1) };
+
+        Self(id)
+    }
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct OrientedNode(NonZeroI32);
+
+impl From<Node> for OrientedNode {
+    fn from(node: Node) -> OrientedNode {
+        Self::new(node.0, false)
+    }
+}
+
+impl From<OrientedNode> for Node {
+    fn from(onode: OrientedNode) -> Node {
+        onode.node()
+    }
+}
 
 impl OrientedNode {
     pub fn id(&self) -> u32 {
@@ -62,6 +119,60 @@ impl OrientedNode {
         let id = unsafe { NonZeroI32::new_unchecked(self.0.get() * -1) };
 
         Self(id)
+    }
+}
+
+pub struct Curve<D> {
+    id: CurveId,
+
+    seq_len: usize,
+
+    steps: Vec<OrientedNode>,
+
+    node_set: roaring::RoaringBitmap,
+    path_set: roaring::RoaringBitmap,
+
+    fwd_adj: Vec<OrientedCurve>,
+    rev_adj: Vec<OrientedCurve>,
+
+    step_data: Vec<D>,
+}
+
+impl<D> Curve<D> {
+    pub fn steps(&self) -> &[OrientedNode] {
+        &self.steps
+    }
+
+    pub fn paths(&self) -> impl Iterator<Item = Path> + '_ {
+        self.path_set.iter().map(|i| Path::from(i))
+    }
+
+    pub fn contains_node(&self, node: Node) -> bool {
+        self.node_set.contains(node.ix() as u32)
+    }
+
+    pub fn contains_path(&self, path: Path) -> bool {
+        self.path_set.contains(path.ix() as u32)
+    }
+
+    pub fn first(&self) -> Option<OrientedNode> {
+        self.steps.first().copied()
+    }
+
+    pub fn last(&self) -> Option<OrientedNode> {
+        self.steps.last().copied()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.steps.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.steps.len()
+    }
+
+    pub fn seq_len(&self) -> usize {
+        self.seq_len
     }
 }
 
