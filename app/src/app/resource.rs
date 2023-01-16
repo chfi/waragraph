5299af42d22aa3d715c6e52456072e113eb0b871
Christian Fischer <christian@chfi.se>
2023-01-16 13:24:16 -0600

    some basic data sources

--- app/src/app/resource.rs
+++ app/src/app/resource.rs
@@ -39,6 +39,75 @@ pub struct GraphDataSources {
     path_f32: HashMap<String, Arc<dyn Fn(PathId) -> anyhow::Result<Vec<f32>>>>,
 }
 
+impl GraphDataSources {
+    pub fn tmp_init(graph: &Arc<PathIndex>) -> Self {
+        let mut graph_f32: HashMap<String, GraphDataSourceFn<f32>> =
+            HashMap::default();
+        let mut path_f32: HashMap<String, PathDataSourceFn<f32>> =
+            HashMap::default();
+
+        // graph node ids
+        {
+            let name = "graph_node_ids".to_string();
+            let graph = graph.clone();
+            let ctor =
+                move || Ok((0..graph.node_count).map(|i| i as f32).collect());
+
+            graph_f32.insert(name, Arc::new(ctor));
+        }
+
+        // path depth
+        {
+            let name = "path_depth".to_string();
+            let graph = graph.clone();
+            let ctor = move |path: PathId| {
+                let mut path_data: BTreeMap<Node, f32> = BTreeMap::default();
+                for step in graph.path_steps[path.ix()].iter() {
+                    *path_data.entry(step.node()).or_default() += 1.0;
+                }
+                let path_data =
+                    path_data.into_iter().map(|(_, v)| v).collect::<Vec<_>>();
+                Ok(path_data)
+            };
+
+            path_f32.insert(name, Arc::new(ctor));
+        }
+
+        // path strand
+        {
+            let name = "path_strand".to_string();
+            let graph = graph.clone();
+            let ctor = move |path: PathId| {
+                let path_steps = &graph.path_steps[path.ix()];
+                let node_set = &graph.path_node_sets[path.ix()];
+                let mut step_count = vec![0.0; node_set.len() as usize];
+                let mut path_data = vec![0.0; node_set.len() as usize];
+
+                for step in path_steps {
+                    let node = step.node();
+                    let d = if step.is_reverse() { 1.0 } else { 0.0 };
+
+                    path_data[node.ix()] += d;
+                    step_count[node.ix()] += 1.0;
+                }
+
+                for (val, count) in path_data.iter_mut().zip(step_count) {
+                    *val = *val / count;
+                }
+
+                Ok(path_data)
+            };
+
+            path_f32.insert(name, Arc::new(ctor));
+        }
+
+        Self {
+            graph_f32,
+            path_f32,
+        }
+    }
+}
+
 pub struct GraphDataStore {
     name_index_map: HashMap<String, StoreIndex>,
 
