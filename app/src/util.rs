4c3c269290c98e1b2fe7b53120e6ce6e9e1b5256
Christian Fischer <christian@chfi.se>
2023-01-24 11:06:45 -0600

    utility struct for uniform buffers

--- app/src/util.rs
+++ app/src/util.rs
@@ -1,3 +1,5 @@
+use wgpu::util::DeviceExt;
+
 #[derive(Debug)]
 pub struct BufferDesc {
     pub buffer: wgpu::Buffer,
@@ -8,4 +10,74 @@ impl BufferDesc {
     pub fn new(buffer: wgpu::Buffer, size: usize) -> Self {
         Self { buffer, size }
     }
+}
+
+pub struct Uniform<T, const N: usize> {
+    pub name: String,
+    data: T,
+
+    buffer: wgpu::Buffer,
+
+    to_bytes: Box<dyn Fn(&T) -> [u8; N]>,
+    need_write: bool,
+}
+
+impl<T, const N: usize> Uniform<T, N> {
+    pub fn new(
+        state: &raving_wgpu::State,
+        usage: wgpu::BufferUsages,
+        name: &str,
+        data: T,
+        to_bytes: impl Fn(&T) -> [u8; N] + 'static,
+    ) -> anyhow::Result<Self> {
+        let to_bytes = Box::new(to_bytes);
+        let name = name.to_string();
+
+        let buf_data = to_bytes(&data);
+
+        let label_str = format!("Uniform: {name}");
+
+        let buffer = state.device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some(&label_str),
+                contents: bytemuck::cast_slice(&buf_data),
+                usage,
+            },
+        );
+
+        Ok(Self {
+            name,
+            data,
+            buffer,
+            to_bytes,
+            need_write: false,
+        })
+    }
+
+    pub fn data_ref(&self) -> &T {
+        &self.data
+    }
+
+    pub fn buffer(&self) -> &wgpu::Buffer {
+        &self.buffer
+    }
+
+    pub fn buffer_size(&self) -> usize {
+        N
+    }
+
+    pub fn update_data(&mut self, f: impl FnOnce(&mut T)) {
+        f(&mut self.data);
+        self.need_write = true;
+    }
+
+    pub fn write_buffer(&mut self, state: &raving_wgpu::State) {
+        if self.need_write {
+            let data = (self.to_bytes)(&self.data);
+            state.queue.write_buffer(&self.buffer, 0, data.as_slice());
+            self.need_write = false;
+        }
+    }
+
+    //
 }
