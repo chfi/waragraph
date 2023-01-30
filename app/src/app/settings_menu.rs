77f1cf740f6d8279ec1e887b92a4e08661185e87
Christian Fischer <christian@chfi.se>
2023-01-30 13:55:47 -0600

    core of settings window

--- app/src/app/settings_menu.rs
+++ app/src/app/settings_menu.rs
@@ -1,18 +1,41 @@
 use std::collections::BTreeMap;
 
-pub struct AppSettings {
-    tabs: BTreeMap<String, ()>,
-    //
-}
+// pub struct AppSettings {
+// }
 
-pub struct SettingsTab {
-    //
+// pub struct SettingsTab {
+//     //
+// }
+
+struct SettingsHandler {
+    name: String,
+    show: Box<dyn Fn(&mut egui::Ui) + Send + Sync + 'static>,
+    // validate: Option<Box<dyn Fn() -> bool + Send + Sync + 'static>;
 }
 
 pub struct SettingsWindow {
-    //
+    handlers: Vec<SettingsHandler>,
 }
 
 impl SettingsWindow {
-    // pub fn show(&self, ctx: &egui::Context) ->
+    pub fn register_widget(
+        &mut self,
+        name: &str,
+        show: impl Fn(&mut egui::Ui) + Send + Sync + 'static,
+    ) {
+        let h = SettingsHandler {
+            name: name.to_string(),
+            show: Box::new(show),
+        };
+
+        self.handlers.push(h);
+    }
+
+    pub fn show(&self, ctx: &egui::Context) {
+        egui::Window::new("Settings").show(ctx, |ui| {
+            for h in self.handlers.iter() {
+                ui.collapsing(&h.name, &h.show);
+            }
+        });
+    }
 }
--- app/src/app/workspace.rs
+++ app/src/app/workspace.rs
@@ -17,13 +17,36 @@ impl Workspace {
 
 impl egui::Widget for &mut Workspace {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        let enabled = self.tsv_path.is_none();
+        ui.horizontal(|ui| {
+            ui.label("GFA:");
+            let mut gfa_buf =
+                self.gfa_path.clone().to_string_lossy().to_string();
+            ui.add_enabled(false, egui::TextEdit::singleline(&mut gfa_buf));
 
-        let files = egui_file::FileDialog::open_file(self.tsv_path.clone())
-            .filter(Box::new(|p: &std::path::Path| p.ends_with(".tsv")));
+            ui.end_row();
 
-        // let mut gfa_buf = self.gfa_path.clone().to_string_lossy().to_string();
-        // ui.add_enabled(false, egui::TextEdit::singleline(&mut
-        todo!();
+            let enabled = self.tsv_path.is_none();
+            let mut tsv_buf = self
+                .tsv_path
+                .as_ref()
+                .map(|tsv| tsv.to_string_lossy().to_string())
+                .unwrap_or_default();
+            ui.add_enabled(enabled, egui::TextEdit::singleline(&mut tsv_buf));
+
+            if ui.button("Choose").clicked() {
+                let mut files =
+                    egui_file::FileDialog::open_file(self.tsv_path.clone())
+                        .filter(Box::new(|p: &std::path::Path| {
+                            p.ends_with(".tsv")
+                        }));
+
+                files.show(ui.ctx());
+
+                if let Some(path) = files.path() {
+                    self.tsv_path = Some(path);
+                }
+            }
+        })
+        .response
     }
 }
