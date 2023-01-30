eccb483042d301a9418d705b5aef871110ee76bf
Christian Fischer <christian@chfi.se>
2023-01-30 14:48:08 -0600

    file picker not working

--- app/src/app/workspace.rs
+++ app/src/app/workspace.rs
@@ -1,4 +1,8 @@
 use std::path::PathBuf;
+use std::sync::Arc;
+
+use egui::{mutex::Mutex, Context, Id};
+use egui_file::FileDialog;
 
 pub struct Workspace {
     pub(super) gfa_path: PathBuf,
@@ -17,7 +21,27 @@ impl Workspace {
 
 impl egui::Widget for &mut Workspace {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        ui.horizontal_wrapped(|ui| {
+        let id = egui::Id::new("Settings_Workspace");
+
+        let mut state =
+            WorkspaceWidgetState::load(ui.ctx(), id).unwrap_or_default();
+
+        let file_picker_open = {
+            let mut lock = state.open_file_dialog.lock();
+            if let Some(dialog) = lock.as_mut() {
+                if dialog.show(ui.ctx()).selected() {
+                    if let Some(path) = dialog.path() {
+                        self.tsv_path = Some(path);
+                    }
+                }
+
+                true
+            } else {
+                false
+            }
+        };
+
+        let resp = ui.horizontal_wrapped(|ui| {
             ui.label("GFA:");
             let mut gfa_buf =
                 self.gfa_path.clone().to_string_lossy().to_string();
@@ -25,7 +49,7 @@ impl egui::Widget for &mut Workspace {
 
             ui.end_row();
 
-            let enabled = self.tsv_path.is_none();
+            let enabled = self.tsv_path.is_none() && !file_picker_open;
             let mut tsv_buf = self
                 .tsv_path
                 .as_ref()
@@ -36,19 +60,34 @@ impl egui::Widget for &mut Workspace {
             ui.add_enabled(enabled, egui::TextEdit::singleline(&mut tsv_buf));
 
             if ui.button("Choose").clicked() {
-                let mut files =
+                let files =
                     egui_file::FileDialog::open_file(self.tsv_path.clone())
                         .filter(Box::new(|p: &std::path::Path| {
                             p.ends_with(".tsv")
                         }));
 
-                files.show(ui.ctx());
-
-                if let Some(path) = files.path() {
-                    self.tsv_path = Some(path);
-                }
+                let mut lock = state.open_file_dialog.lock();
+                *lock = Some(files);
             }
-        })
-        .response
+        });
+
+        state.store(ui.ctx(), id);
+
+        resp.response
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct WorkspaceWidgetState {
+    open_file_dialog: Arc<Mutex<Option<FileDialog>>>,
+}
+
+impl WorkspaceWidgetState {
+    pub fn load(ctx: &Context, id: Id) -> Option<Self> {
+        ctx.data().get_temp(id)
+    }
+
+    pub fn store(self, ctx: &Context, id: Id) {
+        ctx.data().insert_temp(id, self);
     }
 }
