95fab1e5680a423eb199e9bd1eaf4cfb6088283d
Christian Fischer <christian@chfi.se>
2023-03-01 14:12:10 -0600

    bit more cleanup

--- lib/src/graph/matrix.rs
+++ lib/src/graph/matrix.rs
@@ -1,12 +1,4 @@
-use crate::graph::{Edge, Node, OrientedNode};
-use roaring::RoaringBitmap;
-use sprs::{CsMat, CsMatBase, CsVec};
-use std::{
-    collections::{BTreeSet, HashMap, HashSet},
-    sync::Arc,
-};
-
-// use super::hyper::{Cycle, HyperSpokeGraph, VertexId};
+use sprs::CsMat;
 
 #[derive(Debug, Clone)]
 pub struct MatGraph<VData, EData> {
@@ -32,38 +24,8 @@ impl<V, E> MatGraph<V, E> {
         sprs::visu::print_nnz_pattern(self.inc.view());
     }
 
-    pub fn from_edges(
-        // vertex_count: usize,
-        // edge_count: usize,
-        edges: impl IntoIterator<Item = (u32, u32)>,
-        vert_data: impl IntoIterator<Item = V>,
-        edge_data: impl IntoIterator<Item = E>,
-    ) -> Self {
-        use sprs::TriMat;
-
-        let vertex = vert_data.into_iter().collect::<Vec<_>>();
-        let edge = edge_data.into_iter().collect::<Vec<_>>();
-
-        let vertex_count = vertex.len();
-
-        // let mut adj = TriMat::new(
-
-        // let mut adj
-
-        todo!();
-    }
-
     pub fn neighbors(&self, vertex: usize) -> Vec<usize> {
         let col = self.adj.outer_view(vertex).unwrap();
         col.indices().to_vec()
-    }
-
-    pub fn neighbors_old(&self, vertex: usize) -> Vec<usize> {
-        let n = self.vertex_count;
-        let v = CsVec::new(n, vec![vertex as usize], vec![1]);
-
-        let ns = &self.adj * &v;
-        let (indices, _) = ns.into_raw_storage();
-        indices
     }
 }
