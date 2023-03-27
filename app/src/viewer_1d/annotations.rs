a05cae30d7a5a2a6c54a89d726f807e8424e5f39
Christian Fischer <christian@chfi.se>
2023-03-27 15:11:08 +0300

    geometry utility for 1d annots

--- app/src/viewer_1d/annotations.rs
+++ app/src/viewer_1d/annotations.rs
@@ -5,7 +5,7 @@ use rstar::{
     RTree,
 };
 use ultraviolet::Vec2;
-use waragraph_core::graph::{Bp, PathId};
+use waragraph_core::graph::{Bp, PathId, PathIndex};
 
 use super::view::View1D;
 
@@ -34,7 +34,9 @@ pub struct AnnotSlot {
 }
 
 impl AnnotSlot {
-    fn new(
+    /// Initializes an annotation slot given items in pangenome space.
+    ///
+    fn new_from_pangenome_space(
         annotations: impl IntoIterator<Item = (std::ops::Range<Bp>, egui::Shape)>,
     ) -> Self {
         let mut annot_objs = Vec::new();
@@ -58,9 +60,51 @@ impl AnnotSlot {
         }
     }
 
+    /// Initializes an annotation slot given items in path space.
+    /// The path ranges to pangenome space, splitting them if
+    /// necessary.
+    fn new_from_path_space(
+        graph: &PathIndex,
+        annotations: impl IntoIterator<
+            Item = (PathId, std::ops::Range<Bp>, egui::Shape),
+        >,
+    ) -> Self {
+        // let mut annot_objs = Vec::new();
+        // let mut shapes = Vec::new();
+
+        for (a_id, (path, path_range, shape)) in
+            annotations.into_iter().enumerate()
+        {
+            todo!();
+
+            // use path index to split path_range into pangenome range chunks
+            // all chunks get the same a_id
+
+            /*
+            let geom =
+                Line::new((range.start.0 as i64,), (range.end.0 as i64,));
+            annot_objs.push(GeomWithData::new(geom, a_id));
+            shapes.push(shape);
+            */
+        }
+
+        // let anchors = vec![None; annot_objs.len()];
+
+        // let annots = RTree::<AnnotsTreeObj>::bulk_load(annot_objs);
+
+        // Self {
+        //     annots,
+        //     shapes,
+        //     anchors,
+        // }
+        todo!();
+    }
+
     fn draw(&self, painter: &egui::Painter, view: &View1D) {
         use rstar::AABB;
         let rect = painter.clip_rect();
+        let rleft = rect.left() as f64;
+        let rright = rect.right() as f64;
 
         let range = view.range();
         // let start = [Bp(range.start), Bp(range.start)];
@@ -75,6 +119,10 @@ impl AnnotSlot {
             let a_id = line.data;
             let left = Bp(line.geom().from.0 as u64);
             let right = Bp(line.geom().to.0 as u64);
+
+            let l_f = left.0 as f64;
+            let r_f = right.0 as f64;
+            // let lf = rect.left() as f64
 
             //
         }
@@ -97,4 +145,47 @@ impl AnnotSlot {
 
         todo!();
     }
+}
+
+// returns the range of valid anchor points along the x-axis, in
+// screen space, of the `pan_range` range under the transformation
+// induced by the `view` and `screen_interval`
+//
+// if the intersection of `view` and `pan_range` is empty, None is returned
+fn anchor_interval(
+    view: &View1D,
+    pan_range: &std::ops::Range<u64>,
+    screen_interval: &std::ops::RangeInclusive<f32>,
+) -> Option<std::ops::RangeInclusive<f32>> {
+    let vrange = view.range();
+    let pleft = pan_range.start;
+    let pright = pan_range.end;
+
+    if pleft > vrange.end || pright < vrange.start {
+        return None;
+    }
+
+    let vl = vrange.start as f32;
+    let vr = vrange.end as f32;
+    let vlen = vr - vl;
+
+    let pl = pleft as f32;
+    let pr = pright as f32;
+
+    let left = pleft.max(vrange.start);
+    let right = pright.min(vrange.end);
+
+    let l = left as f32;
+    let r = right as f32;
+
+    let lt = (l - vl) / vlen;
+    let rt = (r - vr) / vlen;
+
+    let (sleft, sright) = screen_interval.clone().into_inner();
+    let slen = sright - sleft;
+
+    let a_left = sleft + lt * slen;
+    let a_right = sleft + rt * slen;
+
+    Some(a_left..=a_right)
 }
