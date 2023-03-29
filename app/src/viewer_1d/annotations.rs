08dd27737a1ce6bfeb11661e8d47c742bf0b8fbe
Christian Fischer <christian@chfi.se>
2023-03-29 09:55:39 +0300

    (WIP) try to avoid label overlaps

--- app/src/viewer_1d/annotations.rs
+++ app/src/viewer_1d/annotations.rs
@@ -63,11 +63,83 @@ pub fn text_shape<L: ToString>(label: L) -> ShapeFn {
 // space, down from the path-range space
 pub struct AnnotSlot {
     // id: AnnotSlotId
+    // really corresponds to the anchor regions
     annots: RTree<AnnotsTreeObj>,
 
     // annots: BTreeMap<[Bp; 2], usize>,
-    shapes: Vec<ShapeFn>,
+    shape_fns: Vec<ShapeFn>,
+
+    annot_shape_objs: Vec<AnnotObj>,
+    // shape_positions: Vec<Option<Vec2>>,
     anchors: Vec<Option<f64>>,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct AnnotObjPos {
+    pos_now: Vec2,
+    pos_old: Vec2,
+    accel: Vec2,
+}
+
+impl AnnotObjPos {
+    fn at_pos(pos: Vec2) -> Self {
+        Self {
+            pos_now: pos,
+            pos_old: pos,
+            accel: Vec2::zero(),
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+struct AnnotObj {
+    pos: Option<AnnotObjPos>,
+    shape_size: Option<Vec2>,
+}
+
+impl AnnotObj {
+    fn pos(&self) -> Option<Vec2> {
+        self.pos.map(|p| p.pos_now)
+    }
+
+    fn size(&self) -> Option<Vec2> {
+        self.shape_size
+    }
+
+    fn egui_rect(&self) -> Option<egui::Rect> {
+        let pos = self.pos()?;
+        let size = self.size()?;
+        let rect = egui::Rect::from_center_size(
+            egui::pos2(pos.x, pos.y),
+            egui::vec2(size.x, size.y),
+        );
+        Some(rect)
+    }
+
+    fn update_position(&mut self, dt: f32) {
+        if let Some(obj_pos) = self.pos.as_mut() {
+            let vel = obj_pos.pos_now - obj_pos.pos_old;
+            obj_pos.pos_old = obj_pos.pos_now;
+            obj_pos.pos_now = obj_pos.pos_now + vel + obj_pos.accel * dt * dt;
+            obj_pos.accel = Vec2::zero();
+        }
+    }
+
+    fn accelerate(&mut self, acc: Vec2) {
+        if let Some(obj_pos) = self.pos.as_mut() {
+            obj_pos.accel += acc;
+        }
+    }
+
+    fn collides_impl(&self, other: &Self) -> Option<bool> {
+        let a = self.egui_rect()?;
+        let b = other.egui_rect()?;
+        Some(a.intersects(b))
+    }
+
+    fn collides(&self, other: &Self) -> bool {
+        self.collides_impl(other).unwrap_or(false)
+    }
 }
 
 impl AnnotSlot {
@@ -77,23 +149,26 @@ impl AnnotSlot {
         annotations: impl IntoIterator<Item = (std::ops::Range<Bp>, ShapeFn)>,
     ) -> Self {
         let mut annot_objs = Vec::new();
-        let mut shapes = Vec::new();
+        let mut shape_fns = Vec::new();
 
         for (a_id, (range, shape)) in annotations.into_iter().enumerate() {
             let geom =
                 Line::new((range.start.0 as i64, 0), (range.end.0 as i64, 0));
             annot_objs.push(GeomWithData::new(geom, a_id));
-            shapes.push(shape);
+            shape_fns.push(shape);
         }
 
-        let anchors = vec![None; shapes.len()];
+        let annot_shape_objs = vec![AnnotObj::default(); shape_fns.len()];
+
+        let anchors = vec![None; shape_fns.len()];
 
         let annots = RTree::<AnnotsTreeObj>::bulk_load(annot_objs);
 
         Self {
             annots,
-            shapes,
+            shape_fns,
             anchors,
+            annot_shape_objs,
         }
     }
 
@@ -107,12 +182,12 @@ impl AnnotSlot {
         >,
     ) -> Self {
         let mut annot_objs = Vec::new();
-        let mut shapes = Vec::new();
+        let mut shape_fns = Vec::new();
 
         for (a_id, (path, path_range, shape)) in
             annotations.into_iter().enumerate()
         {
-            shapes.push(shape);
+            shape_fns.push(shape);
             let range_end = path_range.end;
             if let Some(steps) = graph.path_step_range_iter(path, path_range) {
                 for (start, step) in steps {
@@ -124,18 +199,86 @@ impl AnnotSlot {
             }
         }
 
-        let anchors = vec![None; shapes.len()];
+        let annot_shape_objs = vec![AnnotObj::default(); shape_fns.len()];
+        let anchors = vec![None; shape_fns.len()];
 
         let annots = RTree::<AnnotsTreeObj>::bulk_load(annot_objs);
 
         Self {
             annots,
-            shapes,
+            shape_fns,
             anchors,
+            annot_shape_objs,
         }
     }
 
-    pub(super) fn draw(&mut self, painter: &egui::Painter, view: &View1D) {
+    pub(super) fn update(&mut self, rect: egui::Rect, view: &View1D, dt: f64) {
+        use rstar::AABB;
+        let rleft = rect.left() as f64;
+        let rright = rect.right() as f64;
+
+        let screen_interval = rleft..=rright;
+
+        let range = view.range();
+
+        let aabb =
+            AABB::from_corners((range.start as i64, 0), (range.end as i64, 0));
+
+        let in_view = self.annots.locate_in_envelope_intersecting(&aabb);
+
+        let mut active = Vec::new();
+
+        for line in in_view {
+            let a_id = line.data;
+            let left = line.geom().from.0 as u64;
+            let right = line.geom().to.0 as u64;
+
+            let reset_anchor = self.anchors[a_id]
+                .map(|x| {
+                    // true if anchor is offscreen
+                    // (could be better; take label size into account)
+                    x < rleft || x > rright
+                })
+                .unwrap_or(true);
+
+            if reset_anchor {
+                // place anchor
+                if let Some(a_range) =
+                    anchor_interval(view, &(left..right), &screen_interval)
+                {
+                    let (l, r) = a_range.into_inner();
+                    let x = l + (r - l) * 0.5;
+                    self.anchors[a_id] = Some(x);
+                }
+            }
+
+            active.push(a_id);
+        }
+
+        active.sort();
+        active.dedup();
+
+        for &i in &active {
+            for &j in &active {
+                if i == j {
+                    continue;
+                }
+
+                let colliding = todo!();
+
+                // if colliding, push apart
+                todo!();
+            }
+        }
+
+        todo!();
+    }
+
+    pub(super) fn draw(&self, painter: &egui::Painter, view: &View1D) {
+        todo!();
+    }
+
+    pub(super) fn draw_old(&mut self, painter: &egui::Painter, view: &View1D) {
         use rstar::AABB;
         let rect = painter.clip_rect();
         let rleft = rect.left() as f64;
@@ -171,7 +314,7 @@ impl AnnotSlot {
             let y = rect.center().y;
             if let Some(x) = self.anchors[a_id] {
                 let pos = egui::pos2(x as f32, y);
-                let shape = (self.shapes[a_id])(painter, pos);
+                let shape = (self.shape_fns[a_id])(painter, pos);
                 painter.add(shape);
             }
         }
