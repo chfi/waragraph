843d233084cff1a1ab4fbe97e479dd3175dab9b0
Christian Fischer <christian@chfi.se>
2023-05-03 10:03:59 -0500

    add node -> path steps map

--- lib/src/graph.rs
+++ lib/src/graph.rs
@@ -1,6 +1,7 @@
 use bimap::BiBTreeMap;
 use roaring::{RoaringBitmap, RoaringTreemap};
 use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::io::prelude::*;
 use std::io::BufReader;
 
@@ -218,6 +219,8 @@ pub struct PathIndex {
 
     pub path_step_offsets: Vec<roaring::RoaringTreemap>,
     pub path_node_sets: Vec<roaring::RoaringBitmap>,
+
+    node_path_steps: Vec<HashMap<PathId, Vec<u32>>>,
 }
 
 pub struct PathStepRangeIter<'a> {
@@ -239,176 +242,6 @@ impl<'a> Iterator for PathStepRangeIter<'a> {
 }
 
 impl PathIndex {
-    pub fn directed_adjacency_matrix(
-        node_count: usize,
-        edges: impl Iterator<Item = Edge>,
-    ) -> sprs::CsMat<u8> {
-        use sprs::TriMat;
-
-        // TODO build the compressed matrix directly
-
-        // node endpoint space
-        let n = node_count * 2;
-
-        let mut mat: TriMat<u8> = TriMat::new((n, n));
-
-        for edge in edges {
-            let (from, to) = edge.endpoints();
-            let fi = from.ix();
-            let ti = to.ix();
-
-            // entry in row i, col j is 1 when there's an edge j->i
-            mat.add_triplet(ti, fi, 1);
-        }
-
-        mat.to_csc()
-    }
-
-    pub fn edges_iter<'a>(&'a self) -> impl Iterator<Item = &'a Edge> {
-        self.edges.iter()
-    }
-
-    pub fn pangenome_len(&self) -> Bp {
-        self.sequence_total_len
-    }
-
-    #[inline]
-    pub fn node_pangenome_range(&self, node: Node) -> std::ops::Range<Bp> {
-        let i = node.0 as u64;
-        let offset = self.segment_offsets.select(i).unwrap_or_default();
-        let next = self
-            .segment_offsets
-            .select(i + 1)
-            .unwrap_or(self.pangenome_len().0);
-
-        Bp(offset)..Bp(next)
-    }
-
-    #[inline]
-    pub fn node_offset_length(&self, node: Node) -> (Bp, Bp) {
-        let i = node.0 as u64;
-        let offset = self.segment_offsets.select(i).unwrap_or_default();
-        let next = self
-            .segment_offsets
-            .select(i + 1)
-            .unwrap_or(self.pangenome_len().0);
-
-        let length = Bp(next - offset);
-        let offset = Bp(offset);
-
-        (offset, length)
-    }
-
-    #[inline]
-    pub fn node_offset(&self, node: Node) -> Bp {
-        self.node_offset_length(node).0
-    }
-
-    pub fn node_at_pangenome_pos(&self, pos: Bp) -> Option<Node> {
-        if pos > self.sequence_total_len {
-            return None;
-        }
-        let rank = self.segment_offsets.rank(pos.0);
-        Some(Node::from(rank as u32))
-    }
-
-    #[inline]
-    pub fn node_length(&self, node: Node) -> Bp {
-        self.node_offset_length(node).1
-    }
-
-    pub fn pos_range_nodes(
-        &self,
-        pos_range: std::ops::Range<u64>,
-    ) -> std::ops::RangeInclusive<Node> {
-        let s = pos_range.start;
-        let e = pos_range.end;
-
-        let start_rank = self.segment_offsets.rank(s);
-        let end_rank = self.segment_offsets.rank(e);
-
-        let first = Node::from(start_rank as usize - 1);
-        let last = Node::from(end_rank as usize - 1);
-
-        first..=last
-    }
-
-    pub fn nodes_pan_range_iter<'i>(
-        &'i self,
-        pan_range: std::ops::Range<u64>,
-    ) -> PangenomeNodePosRangeIter<'i> {
-        PangenomeNodePosRangeIter::new_pos_range(self, pan_range)
-    }
-
-    pub fn path_data_pan_range_iter<'index, 'data, T>(
-        &'index self,
-        pan_range: std::ops::Range<u64>,
-        path_id: PathId,
-        data: &'data [T],
-    ) -> PangenomePathDataPosRangeIter<'index, 'data, T> {
-        PangenomePathDataPosRangeIter::new_pos_range(
-            self, pan_range, path_id, data,
-        )
-    }
-
-    pub fn path_steps<'a>(
-        &'a self,
-        path_name: &str,
-    ) -> Option<&'a [OrientedNode]> {
-        let id = self.path_names.get_by_right(path_name)?;
-        self.path_steps.get(id.ix()).map(|s| s.as_slice())
-    }
-
-    pub fn step_at_pos(
-        &self,
-        path_id: PathId,
-        pos: usize,
-    ) -> Option<OrientedNode> {
-        let offsets = self.path_step_offsets.get(path_id.ix())?;
-        let steps = self.path_steps.get(path_id.ix())?;
-        let pos_rank = offsets.rank(pos as u64) as usize;
-        steps.get(pos_rank).copied()
-    }
-
-    pub fn path_step_range_iter<'a, P: Into<u64>>(
-        &'a self,
-        path_id: PathId,
-        pos_range: std::ops::Range<P>,
-    ) -> Option<PathStepRangeIter<'a>> {
-        let offsets = self.path_step_offsets.get(path_id.ix())?;
-
-        let start = pos_range.start.into();
-        let end = pos_range.end.into();
-        let pos_range = start..end;
-
-        let start_rank = offsets.rank(start);
-        let end_rank = offsets.rank(end);
-
-        let steps = {
-            let path_steps = self.path_steps.get(path_id.ix())?;
-
-            let skip = (start_rank as usize).checked_sub(1).unwrap_or_default();
-            let take = end_rank as usize - skip;
-            let iter = path_steps
-                .iter()
-                .skip(skip)
-                .take(take)
-                .enumerate()
-                .map(move |(ix, step)| (skip + ix, step))
-                .fuse();
-
-            Box::new(iter) as Box<dyn Iterator<Item = _>>
-        };
-
-        Some(PathStepRangeIter {
-            path_id,
-            pos_range,
-            steps,
-            // first_step_start_pos,
-            // last_step_end_pos,
-        })
-    }
-
     pub fn from_gfa(
         gfa_path: impl AsRef<std::path::Path>,
     ) -> std::io::Result<Self> {
@@ -507,6 +340,9 @@ impl PathIndex {
         let mut path_node_sets: Vec<RoaringBitmap> = Vec::new();
         // let mut path_pos: Vec<Vec<usize>> = Vec::new();
 
+        let mut node_path_steps: Vec<HashMap<PathId, Vec<u32>>> =
+            vec![HashMap::default(); node_count];
+
         loop {
             line_buf.clear();
 
@@ -533,7 +369,8 @@ impl PathIndex {
             let name = std::str::from_utf8(name).map_err(|e| {
                 std::io::Error::new(std::io::ErrorKind::InvalidData, e)
             })?;
-            path_names.insert(PathId::from(path_steps.len()), name.to_string());
+            let path_id = PathId::from(path_steps.len());
+            path_names.insert(path_id, name.to_string());
 
             let mut pos = 0;
 
@@ -556,9 +393,16 @@ impl PathIndex {
 
                 let step = OrientedNode::new(seg_ix as u32, is_rev);
 
+                let step_index = parsed_steps.len();
+
                 parsed_steps.push(step);
                 offsets.push(pos as u64);
                 path_nodes.insert(seg_ix);
+
+                node_path_steps[step.node().ix()]
+                    .entry(path_id)
+                    .or_default()
+                    .push(step_index as u32);
 
                 pos += len;
             }
@@ -574,12 +418,204 @@ impl PathIndex {
             path_step_offsets,
             path_node_sets,
 
+            node_path_steps,
+
             segment_offsets,
             node_count,
             segment_id_range: seg_id_range,
             sequence_total_len: Bp(sequence_total_len as u64),
 
             edges,
+        })
+    }
+
+    pub fn directed_adjacency_matrix(
+        node_count: usize,
+        edges: impl Iterator<Item = Edge>,
+    ) -> sprs::CsMat<u8> {
+        use sprs::TriMat;
+
+        // TODO build the compressed matrix directly
+
+        // node endpoint space
+        let n = node_count * 2;
+
+        let mut mat: TriMat<u8> = TriMat::new((n, n));
+
+        for edge in edges {
+            let (from, to) = edge.endpoints();
+            let fi = from.ix();
+            let ti = to.ix();
+
+            // entry in row i, col j is 1 when there's an edge j->i
+            mat.add_triplet(ti, fi, 1);
+        }
+
+        mat.to_csc()
+    }
+
+    pub fn edges_iter<'a>(&'a self) -> impl Iterator<Item = &'a Edge> {
+        self.edges.iter()
+    }
+
+    pub fn pangenome_len(&self) -> Bp {
+        self.sequence_total_len
+    }
+
+    #[inline]
+    pub fn node_pangenome_range(&self, node: Node) -> std::ops::Range<Bp> {
+        let i = node.0 as u64;
+        let offset = self.segment_offsets.select(i).unwrap_or_default();
+        let next = self
+            .segment_offsets
+            .select(i + 1)
+            .unwrap_or(self.pangenome_len().0);
+
+        Bp(offset)..Bp(next)
+    }
+
+    #[inline]
+    pub fn node_offset_length(&self, node: Node) -> (Bp, Bp) {
+        let i = node.0 as u64;
+        let offset = self.segment_offsets.select(i).unwrap_or_default();
+        let next = self
+            .segment_offsets
+            .select(i + 1)
+            .unwrap_or(self.pangenome_len().0);
+
+        let length = Bp(next - offset);
+        let offset = Bp(offset);
+
+        (offset, length)
+    }
+
+    #[inline]
+    pub fn node_offset(&self, node: Node) -> Bp {
+        self.node_offset_length(node).0
+    }
+
+    pub fn node_at_pangenome_pos(&self, pos: Bp) -> Option<Node> {
+        if pos > self.sequence_total_len {
+            return None;
+        }
+        let rank = self.segment_offsets.rank(pos.0);
+        Some(Node::from(rank as u32))
+    }
+
+    #[inline]
+    pub fn node_length(&self, node: Node) -> Bp {
+        self.node_offset_length(node).1
+    }
+
+    /// Returns each occurrence of the given node in the path
+    /// as its step index
+    pub fn node_path_steps<'a>(
+        &'a self,
+        node: Node,
+        path: PathId,
+    ) -> Option<impl Iterator<Item = usize> + 'a> {
+        let node_occs = self.node_path_steps.get(node.ix())?;
+        node_occs
+            .get(&path)
+            .map(|occs| occs.iter().map(|&i| i as usize))
+    }
+
+    pub fn node_on_path(&self, node: Node, path: PathId) -> bool {
+        self.node_path_steps
+            .get(node.ix())
+            .map(|occs| occs.contains_key(&path))
+            .unwrap_or(false)
+    }
+
+    pub fn pos_range_nodes(
+        &self,
+        pos_range: std::ops::Range<u64>,
+    ) -> std::ops::RangeInclusive<Node> {
+        let s = pos_range.start;
+        let e = pos_range.end;
+
+        let start_rank = self.segment_offsets.rank(s);
+        let end_rank = self.segment_offsets.rank(e);
+
+        let first = Node::from(start_rank as usize - 1);
+        let last = Node::from(end_rank as usize - 1);
+
+        first..=last
+    }
+
+    pub fn nodes_pan_range_iter<'i>(
+        &'i self,
+        pan_range: std::ops::Range<u64>,
+    ) -> PangenomeNodePosRangeIter<'i> {
+        PangenomeNodePosRangeIter::new_pos_range(self, pan_range)
+    }
+
+    pub fn path_data_pan_range_iter<'index, 'data, T>(
+        &'index self,
+        pan_range: std::ops::Range<u64>,
+        path_id: PathId,
+        data: &'data [T],
+    ) -> PangenomePathDataPosRangeIter<'index, 'data, T> {
+        PangenomePathDataPosRangeIter::new_pos_range(
+            self, pan_range, path_id, data,
+        )
+    }
+
+    pub fn path_steps<'a>(
+        &'a self,
+        path_name: &str,
+    ) -> Option<&'a [OrientedNode]> {
+        let id = self.path_names.get_by_right(path_name)?;
+        self.path_steps.get(id.ix()).map(|s| s.as_slice())
+    }
+
+    pub fn step_at_pos(
+        &self,
+        path_id: PathId,
+        pos: usize,
+    ) -> Option<OrientedNode> {
+        let offsets = self.path_step_offsets.get(path_id.ix())?;
+        let steps = self.path_steps.get(path_id.ix())?;
+        let pos_rank = offsets.rank(pos as u64) as usize;
+        steps.get(pos_rank).copied()
+    }
+
+    pub fn path_step_range_iter<'a, P: Into<u64>>(
+        &'a self,
+        path_id: PathId,
+        pos_range: std::ops::Range<P>,
+    ) -> Option<PathStepRangeIter<'a>> {
+        let offsets = self.path_step_offsets.get(path_id.ix())?;
+
+        let start = pos_range.start.into();
+        let end = pos_range.end.into();
+        let pos_range = start..end;
+
+        let start_rank = offsets.rank(start);
+        let end_rank = offsets.rank(end);
+
+        let steps = {
+            let path_steps = self.path_steps.get(path_id.ix())?;
+
+            let skip = (start_rank as usize).checked_sub(1).unwrap_or_default();
+            let take = end_rank as usize - skip;
+            let iter = path_steps
+                .iter()
+                .skip(skip)
+                .take(take)
+                .enumerate()
+                .map(move |(ix, step)| (skip + ix, step))
+                .fuse();
+
+            Box::new(iter) as Box<dyn Iterator<Item = _>>
+        };
+
+        Some(PathStepRangeIter {
+            path_id,
+            pos_range,
+            steps,
+            // first_step_start_pos,
+            // last_step_end_pos,
         })
     }
 }
