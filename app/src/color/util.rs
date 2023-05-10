95693a8dd1cbe20a722894f940bc5ad2762d00b0
Christian Fischer <christian@chfi.se>
2023-05-10 11:07:53 -0500

    change hash crate to fix colors

--- app/src/color/util.rs
+++ app/src/color/util.rs
@@ -1,11 +1,20 @@
-pub fn path_name_hash_color(path_name: &str) -> [f32; 3] {
-    use sha256::digest;
-    let hashed = digest(path_name);
-    let hashed = hashed.as_bytes();
+fn hashed_rgb(name: &str) -> [u8; 3] {
+    use sha2::Digest;
+    use sha2::Sha256;
 
-    let path_r = hashed[24];
-    let path_g = hashed[8];
-    let path_b = hashed[16];
+    let mut hasher = Sha256::new();
+    hasher.update(name.as_bytes());
+    let hash = hasher.finalize();
+
+    let r = hash[24];
+    let g = hash[8];
+    let b = hash[16];
+
+    [r, g, b]
+}
+
+pub fn path_name_hash_color_alt(path_name: &str) -> [f32; 3] {
+    let [path_r, path_g, path_b] = hashed_rgb(path_name);
 
     let r_f = (path_r as f32) / std::u8::MAX as f32;
     let g_f = (path_g as f32) / std::u8::MAX as f32;
@@ -14,4 +23,30 @@ pub fn path_name_hash_color(path_name: &str) -> [f32; 3] {
     let sum = r_f + g_f + b_f;
 
     [r_f / sum, g_f / sum, b_f / sum]
+}
+
+pub fn path_name_hash_color(path_name: &str) -> [f32; 3] {
+    let [path_r, path_g, path_b] = hashed_rgb(path_name);
+
+    let r_f = (path_r as f32) / std::u8::MAX as f32;
+    let g_f = (path_g as f32) / std::u8::MAX as f32;
+    let b_f = (path_b as f32) / std::u8::MAX as f32;
+
+    let sum = r_f + g_f + b_f;
+
+    let r_f = r_f / sum;
+    let g_f = g_f / sum;
+    let b_f = b_f / sum;
+
+    let f = (1.0 / r_f.max(g_f).max(b_f)).min(1.5);
+
+    let r_u = (255. * (r_f * f).min(1.0)).round();
+    let g_u = (255. * (g_f * f).min(1.0)).round();
+    let b_u = (255. * (b_f * f).min(1.0)).round();
+
+    let r_f = (r_u as f32) / std::u8::MAX as f32;
+    let g_f = (g_u as f32) / std::u8::MAX as f32;
+    let b_f = (b_u as f32) / std::u8::MAX as f32;
+
+    [r_f, g_f, b_f]
 }
