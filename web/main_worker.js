7f2e48e4abfdc9113b9d905d56d708cffa39267b
Christian Fischer <christian@chfi.se>
2023-09-13 16:06:44 -0500

    fix path viewer creation issues, cleanup

--- web/main_worker.js
+++ web/main_worker.js
@@ -14,8 +14,6 @@ let _state;
 
 let wasm;
 
-
-
 class PathViewerCtx {
     constructor(coord_sys, data, { bins, color_0, color_1}) {
         // TODO set view based on coord_sys, or take an optional argument
@@ -27,7 +25,7 @@ class PathViewerCtx {
     }
 
     connectCanvas(offscreen_canvas) {
-        console.log("??????????");
+        console.log(offscreen_canvas);
         this.path_viewer.set_target_canvas(offscreen_canvas);
     }
 
@@ -92,7 +90,7 @@ async function run() {
 
 
     Comlink.expose({
-        createPathViewer(path_name) {
+        createPathViewer(offscreen_canvas, path_name) {
             console.log("in createPathViewer with " + path_name);
             // let path_name = "gi|528476637:29857558-29915771";
             let coord_sys = _state.coord_sys;
@@ -110,6 +108,8 @@ async function run() {
 
             let viewer = new PathViewerCtx(coord_sys, data, opts);
 
+            viewer.connectCanvas(offscreen_canvas);
+
             return Comlink.proxy(viewer);
         },
 
@@ -122,23 +122,6 @@ async function run() {
             _state.path_viewer.sample();
         },
 
-        // path_viewer: Comlink.proxy(_state.path_viewer),
-        // connect_canvas(offscreen_canvas) {
-        //     // _state.path_viewer.transfer_canvas_control_to_self(canvas
-        //     console.log("set_canvas");
-        //     _state.path_viewer.set_canvas(offscreen_canvas);
-        //     console.log("sample_range");
-        //     _state.path_viewer.sample_range(0, 10000);
-        //     console.log("draw_to_canvas");
-        //     _state.path_viewer.draw_to_canvas();
-        //     // console.log("canvas_test");
-        //     // _state.path_viewer.canvas_test();
-        // },
-        // sample_data(left, right, bin_count) {
-        //     _state.path_viewer.sample_range(left, right);
-        //     // let data = _state.path_viewer.get_bin_data();
-        //     return;
-        // },
         get_graph() {
             return Comlink.proxy(_graph);
         }
@@ -148,77 +131,3 @@ async function run() {
 }
 
 run();
-
-/*
-
-wasm_bindgen('./pkg/web_bg.wasm')
-    .then((w) => {
-        console.log("done???");
-        console.log(w);
-
-        console.log(wasm_bindgen);
-
-        const gfa_path = '../data/A-3105.fa.353ea42.34ee7b1.1576367.smooth.fix.gfa';
-        // const tsv_path = '../data/A-3105.layout.tsv';
-
-        console.log("fetching GFA");
-
-        let gfa = fetch(gfa_path);
-        // let tsv = fetch(tsv_path);
-
-        
-        console.log("parsing GFA");
-
-        // let ctx = wasm_bindgen.initialize_with_data_fetch(gfa, tsv
-        let graph = wasm_bindgen.load_gfa_path_index(gfa);
-
-        // Comlink.expose(wasm_bindgen);
-
-        return graph;
-    })
-    .then((graph) => {
-        console.log("GFA loaded");
-        console.log("exposing interface");
-        console.log(graph);
-        console.log(graph.node_count());
-
-        let path_name = "gi|528476637:29857558-29915771";
-        let coord_sys = wasm_bindgen.CoordSys.global_from_graph(graph);
-        let data = wasm_bindgen.generate_depth_data(graph, path_name);
-
-        _state = { coord_sys, path_name, data };
-
-        console.log(_state);
-
-        _graph = graph;
-        console.log("worker node count: " + _graph.node_count());
-
-        postMessage("graph-ready");
-
-    });
-
-const obj = {
-  counter: 0,
-  inc() {
-    this.counter++;
-  },
-};
-
-Comlink.expose({
-    sample_data(left, right, bin_count) {
-        let bins = new Float32Array(bin_count);
-        _state.coord_sys.sample_range(left, right, _state.data, bins);
-        return Comlink.transfer(bins, [bins.buffer]);
-    },
-    get_graph() {
-        return Comlink.proxy(_graph);
-    }
-});
-*/
-
-// Comlink.expose {
-//     __graph,
-//     node_count() {
-//     }
-// }
-
