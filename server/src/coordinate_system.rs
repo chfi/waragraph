3a821e2bb44dccc324746a19e192e18bd0b81081
Christian Fischer <christian@chfi.se>
2024-02-07 11:25:21 -0600

    (WIP) offload more of annotation prep to the server

--- server/src/coordinate_system.rs
+++ server/src/coordinate_system.rs
@@ -7,10 +7,11 @@ use arrow2::array::{
 use arrow2::chunk::Chunk;
 use arrow2::datatypes::DataType;
 use arrow2::io::ipc::write::{FileWriter, WriteOptions};
-use rocket::serde::{json::Json, Serialize};
+use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::{get, post, State};
 use waragraph_core::arrow_graph::ArrowGFA;
 use waragraph_core::coordinate_system::CoordSys;
+use waragraph_core::graph_layout::GraphLayout;
 
 #[get("/global/segment_at_offset?<pos_bp>")]
 pub async fn global_segment_at_offset(
@@ -78,27 +79,13 @@ pub async fn global(coord_sys_cache: &State<crate::CoordSysCache>) -> Vec<u8> {
 //     todo!();
 // }
 
-#[get("/path_interval_to_global_blocks?<path_id>&<start_bp>&<end_bp>")]
-pub async fn path_interval_to_global_blocks(
-    graph: &State<Arc<ArrowGFA>>,
-    coord_sys_cache: &State<crate::CoordSysCache>,
+pub async fn path_interval_to_global_blocks_impl(
+    graph: &Arc<ArrowGFA>,
+    path_cs: Arc<CoordSys>,
     path_id: u32,
     start_bp: u64,
     end_bp: u64,
 ) -> Vec<u8> {
-    // let global_cs = coord_sys_cache
-    //     .map
-    //     .read()
-    //     .await
-    //     .get("global")
-    //     .unwrap()
-    //     .clone();
-
-    let path_cs = coord_sys_cache
-        .get_or_compute_for_path(graph, path_id)
-        .await
-        .unwrap();
-
     let step_range = path_cs.bp_to_step_range(start_bp, end_bp);
 
     let steps = graph.path_steps(path_id);
@@ -134,7 +121,25 @@ pub async fn path_interval_to_global_blocks(
     }
 
     out
-    // bytemuck::cast_vec(ranges)
+}
+
+#[get("/path_interval_to_global_blocks?<path_id>&<start_bp>&<end_bp>")]
+pub async fn path_interval_to_global_blocks(
+    graph: &State<Arc<ArrowGFA>>,
+    coord_sys_cache: &State<crate::CoordSysCache>,
+    path_id: u32,
+    start_bp: u64,
+    end_bp: u64,
+) -> Vec<u8> {
+    let path_cs = coord_sys_cache
+        .get_or_compute_for_path(graph, path_id)
+        .await
+        .unwrap();
+
+    path_interval_to_global_blocks_impl(
+        graph, path_cs, path_id, start_bp, end_bp,
+    )
+    .await
 }
 
 // TODO the return type of this and so many other endpoints
@@ -151,4 +156,77 @@ pub async fn get_segment_at_path_position(
         .unwrap();
     let seg = path_cs.segment_at_pos(pos_bp);
     Json(seg)
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct AnnotationRange {
+    path_id: u32,
+
+    start_bp: u64,
+    end_bp: u64,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct PreparedAnnotation {
+    first_step: u32,
+    last_step: u32,
+
+    start_world_x: f32,
+    start_world_y: f32,
+    end_world_x: f32,
+    end_world_y: f32,
+
+    blocks_1d_bp: Vec<u64>,
+}
+
+// #[post("/prepare_annotation_record/<coord_sys>", data = "<record>")]
+#[post("/prepare_annotation_record", data = "<records>")]
+pub async fn prepare_annotation_record(
+    graph: &State<Arc<ArrowGFA>>,
+    coord_sys_cache: &State<crate::CoordSysCache>,
+    graph_layout: &State<GraphLayout>,
+    records: Json<Vec<AnnotationRange>>,
+    // coord_sys: String,
+) -> Option<Json<Vec<PreparedAnnotation>>> {
+    let cs = {
+        let cs_map = coord_sys_cache.map.read().await;
+        cs_map.get("<global>")?.clone()
+    };
+
+    let mut result = Vec::new();
+
+    for record in records.0 {
+        let path_steps = graph.path_steps(record.path_id);
+
+        // TODO should probably not fail the entire request here
+        let step_range = cs.bp_to_step_range(record.start_bp, record.end_bp);
+        let first_step = path_steps.get(*step_range.start())?;
+        let last_step = path_steps.get(*step_range.end())?;
+
+        let [x0, y0, ..] = graph_layout.segment_position(first_step)?;
+        let [.., x1, y1] = graph_layout.segment_position(last_step)?;
+
+        let path_cs = coord_sys_cache
+            .get_or_compute_for_path(graph, record.path_id)
+            .await?;
+
+        let blocks_1d_bp = path_interval_to_global_blocks_impl(
+            graph, path_cs, path_id, start_bp, end_bp,
+        )
+        .await;
+
+        result.push(PreparedAnnotation {
+            first_step,
+            last_step,
+            start_world_x: x0,
+            start_world_y: y0,
+            end_world_x: x1,
+            end_world_y: y1,
+            blocks_1d_bp,
+        });
+    }
+
+    Some(Json(result))
 }
