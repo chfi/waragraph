f7da99eb2f158afe499897de707c846cbe09e1e4
Christian Fischer <christian@chfi.se>
2024-03-26 15:53:59 -0500

    (WIP) encapsulate SVG annotation display logic

---
+++ web/ts/svg_annotations.ts
@@ -0,0 +1,32 @@
+
+import { Color, Scale } from 'chroma-js';
+
+
+class SvgAnnotationHandler {
+  //
+
+  constructor() {
+    // prepare SVG viewport
+  }
+
+  onResize() {
+    // update SVG viewport;
+    // if necessary apply SVG tree
+  }
+
+  async highlightSubpath(
+    path: string | number,
+    start: number | BigInt,
+    end: number | BigInt,
+    color: Color,
+    width: number) {
+
+
+      // use path offsets to get path step range, including
+      // offsets for first & last step
+
+      // 
+
+
+    }
+}
